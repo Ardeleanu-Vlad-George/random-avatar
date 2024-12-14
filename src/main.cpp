8bb950d5dfb47c7056347f69cc1471323124fa07
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-14 21:43:00 +0200

    Replaced the 'magic numbers' with defines

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,46 @@
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
 
-#define CELL_SIZE 84
+#define CELL_SIZE   84
+#define CELL_COUNT  7
 
 int main(){
-  int mask[5][5];
-  for(int i=0; i < 25; i++)
-    mask[i/5][i%5]=0;
+  int mask[CELL_COUNT][CELL_COUNT];
+  for(int i=0; i < CELL_COUNT*CELL_COUNT; i++)
+    mask[i/CELL_COUNT][i%CELL_COUNT]=0;
   srand(time(NULL));
 
   //generate first half
-  for(int i=0; i < 5; i++)
-    for(int j=0; j < 2; j++)
+  for(int i=0; i < CELL_COUNT; i++)
+    for(int j=0; j < CELL_COUNT/2+1; j++)
       mask[i][j]=rand()%3;
 
   //generate middle part
-  for(int i=0; i < 5; i++)
-    mask[i][2]=rand()%3;
+  for(int i=0; i < CELL_COUNT; i++)
+    mask[i][CELL_COUNT/2+1]=rand()%3;
 
   //mirror the first half over the other
-  for(int i=0; i < 5; i++)
-    for(int j=0; j < 2; j++)
-      mask[i][4-j]=mask[i][j];
+  for(int i=0; i < CELL_COUNT; i++)
+    for(int j=0; j < CELL_COUNT/2+1; j++)
+      mask[i][CELL_COUNT-1-j]=mask[i][j];
 
   //start saving into image
 
   sf::Color colors[3];
-  colors[0] = sf::Color(155, 55, 55);//primary
-  colors[1] = sf::Color(55, 155, 55);//secondary
-  colors[2] = sf::Color(55, 55, 155);//accent
+  colors[0] = sf::Color(255, 0, 0);//primary
+  colors[1] = sf::Color(0, 255, 0);//secondary
+  colors[2] = sf::Color(0, 0, 255);//accent
 
   sf::Image img;
-  img.create(5*CELL_SIZE, 5*CELL_SIZE);
+  img.create(CELL_COUNT*CELL_SIZE, CELL_COUNT*CELL_SIZE);
 
-  for(int i=0; i < 5*CELL_SIZE; i++)
-    for(int j=0; j < 5*CELL_SIZE; j++)
+  for(int i=0; i < CELL_COUNT*CELL_SIZE; i++)
+    for(int j=0; j < CELL_COUNT*CELL_SIZE; j++)
       img.setPixel(i, j, colors[mask[j/CELL_SIZE][i/CELL_SIZE]]);
 
   img.saveToFile("data/out.png");
 
-  for(int i=0; i < 25; i++)
-    std::cout<<mask[i/5][i%5]<<(i%5==4 ? '\n' : '\0');
+  for(int i=0; i < CELL_COUNT*CELL_COUNT; i++)
+    std::cout<<mask[i/CELL_COUNT][i%CELL_COUNT]<<(i%CELL_COUNT==CELL_COUNT-1 ? '\n' : '\0');
   return 0;
 }
